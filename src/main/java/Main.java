f6787b444e9892eeff2a9c125ddb380d5aa0a819
Erik <erikpastorrios1994@gmail.com>
2020-05-09 19:34:24 +0200

    Add JAVA.XML API

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, JAXBException {
         // write your code here
 
         System.out.println("Prueba de GitHub actions");
