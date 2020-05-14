93da2d0677292a40a7d53bc427abb522d84c2178
Erik <erikpastorrios1994@gmail.com>
2020-05-14 13:07:40 +0200

    Creación de Test para proyecto

---
+++ src/test/java/ProyectoTest.java
@@ -0,0 +1,96 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProyectoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getCodigoProyecto() {
+    }
+
+    @Test
+    void setCodigoProyecto() {
+    }
+
+    @Test
+    void getNombreProyecto() {
+    }
+
+    @Test
+    void setNombreProyecto() {
+    }
+
+    @Test
+    void getLocalizacion() {
+    }
+
+    @Test
+    void setLocalizacion() {
+    }
+
+    @Test
+    void getLineaAccion() {
+    }
+
+    @Test
+    void setLineaAccion() {
+    }
+
+    @Test
+    void getSublineaAccion() {
+    }
+
+    @Test
+    void setSublineaAccion() {
+    }
+
+    @Test
+    void getFechaInicio() {
+    }
+
+    @Test
+    void setFechaInicio() {
+    }
+
+    @Test
+    void getFechaFin() {
+    }
+
+    @Test
+    void setFechaFin() {
+    }
+
+    @Test
+    void getSocioLocal() {
+    }
+
+    @Test
+    void setSocioLocal() {
+    }
+
+    @Test
+    void getAccionesRealizar() {
+    }
+
+    @Test
+    void setAccionesRealizar() {
+    }
+
+    @Test
+    void getMiembrosEquipo() {
+    }
+
+    @Test
+    void setMiembrosEquipo() {
+    }
+}
