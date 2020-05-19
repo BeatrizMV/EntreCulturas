48679d9c4a31211c497c4c040006b806031d247c
Erik <erikpastorrios1994@gmail.com>
2020-05-19 11:21:19 +0200

    Renombrar archivos para hacer más claro que són

--- src/main/java/daoroot/DaoFactory.java
+++ src/main/java/daoroot/DaoFactory.java
@@ -1,5 +1,16 @@
 package daoroot;
 
-public class DaoFactory {
-    public static final int XML = 1;
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+    public void crearNuevo(T t);
+
+    public Optional<T> obtener(String id);
+
+    public void actualizar(T t, String[] params);
+
+    public void borrar(T t);
+
+    public List<T> obtenerDatos();
 }
--- src/main/java/daoroot/XmlDaoFactory.java
+++ src/main/java/daoroot/XmlDaoFactory.java
@@ -1,4 +1,17 @@
 package daoroot;
 
-public class XmlDaoFactory {
+import java.util.List;
+import java.util.Optional;
+
+public abstract class XmlDao<T> implements Dao<T> {
+
+	public abstract void crearNuevo(T t);
+
+    public abstract Optional<T> obtener(String id);
+
+    public abstract void actualizar(T t, String[] params);
+
+    public abstract void borrar(T t);
+
+    public abstract List<T> obtenerDatos();
 }
