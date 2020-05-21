8cdaa384be458fcb4944fc30464465eeb5c402b2
Erik <erikpastorrios1994@gmail.com>
2020-05-21 22:51:39 +0200

    End of the initial implementation of the actualizarArchivo Method & refactor to use the switch independently for each child

--- src/main/java/daoroot/xml/XMLSedeDAO.java
+++ src/main/java/daoroot/xml/XMLSedeDAO.java
@@ -11,6 +11,7 @@ import javax.xml.bind.Marshaller;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,17 @@ public class XMLSedeDAO extends XMLDAO<Sede> implements DAO<Sede> {
     @Override
     public List<Sede> obtenerDatos() {
         return null;
+    }
+
+    @Override
+    protected void updateFile(Object t, int field, String value) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, DaoException {
+        switch (field) {
+            case 0:
+                throw new DaoException("El ID no se puede modificar");
+            case 1:
+                t.getClass().getMethod("setNombreSede", String.class).invoke(t,value);
+                crearNuevoArchivo((Sede) t);
+                break;
+        }
     }
 }
