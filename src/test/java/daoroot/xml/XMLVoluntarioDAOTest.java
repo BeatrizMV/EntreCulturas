5e2663768d875aaf3584b575fc733dd0b441cd94
Erik <erikpastorrios1994@gmail.com>
2020-05-21 08:51:01 +0200

    Re add files from the rest of the group

---
+++ src/test/java/daoroot/xml/XMLVoluntarioDAOTest.java
@@ -0,0 +1,50 @@
+package daoroot.xml;
+
+import java.util.Optional;
+
+import exceptions.DaoException;
+import org.junit.jupiter.api.Test;
+
+import enums.Rol;
+import root.Direccion;
+import root.Voluntario;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class XMLVoluntarioDAOTest {
+    private final XMLVoluntarioDAO toTest = new XMLVoluntarioDAO();
+
+    private Voluntario createVoluntario() {
+
+        final Voluntario vol = new Voluntario(1, "44444444M", "Ana", "Vidal", "Fernandez", "calle", "Wallaby", 42,
+                "Sidney", 28977, "Spain", "", 666223344, "avidalfern@uoc.edu", "avidalfern", "daotest", Rol.ADMIN);
+        return vol;
+    }
+
+
+    @Test
+    public void xmlGetsCreatedDoesNotRaiseExceptions() throws DaoException {
+        Voluntario voluntario = createVoluntario();
+        toTest.crearNuevo(voluntario);
+    }
+
+    @Test
+    public void xmlGetsReadFromDoesNotRaiseExceptions() throws DaoException {
+        Voluntario voluntario = createVoluntario();
+        toTest.crearNuevo(voluntario);
+        Optional<Voluntario> ret = toTest.obtener(String.valueOf(voluntario.getIdPersona()));
+        assertTrue(ret.isPresent());
+    }
+
+    @Test
+    public void xmlIsDeleted() throws DaoException {
+        Voluntario voluntario = createVoluntario();
+        toTest.crearNuevo(voluntario);
+
+        toTest.borrar(voluntario);
+
+        assertFalse(toTest.obtener(String.valueOf(voluntario.getIdPersona())).isPresent());
+    }
+}
