bb976c08c42f71ed350365754a3d19eb5f22fab0
Beatriz Martínez <beatrizmartinezvelezgti@gmail.com>
2020-08-03 20:46:23 +0200

    Creada Insert Proyecto

--- src/main/java/daoroot/db/DbProyectoDao.java
+++ src/main/java/daoroot/db/DbProyectoDao.java
@@ -4,6 +4,8 @@ import DBConnector.DBConnector;
 import root.Proyecto;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class DbProyectoDao implements  DBDAO<Proyecto> {
@@ -27,6 +29,22 @@ public class DbProyectoDao implements  DBDAO<Proyecto> {
 
     @Override
     public void insertData(Connection connection, Proyecto data) {
+
+        try {
+            Statement stmt = connection.createStatement();
+            String query = "insert into " + TABLA_PROYECTOS + "(" + PROYECTOS_ID + ", " + PROYECTOS_NOMBRE
+                    + "," + PROYECTOS_TIPOVIA + "," + PROYECTOS_VIA + "," + PROYECTOS_NUM + "," + PROYECTOS_PROVINCIA
+                    + "," + PROYECTOS_CP + "," + PROYECTOS_PAIS + "," + PROYECTOS_OBSERVACIONES + "," + PROYECTOS_FECHAINCIO + ","
+                    + PROYECTOS_SOCIOLOCAL +  "," + PROYECTOS_ACCIONES + "," + PROYECTOS_LINEACCION + "," + PROYECTOS_SUBLINEA + ") values ('"
+                    + data.getCodigoProyecto() + "', '" + data.getNombreProyecto()+"' , '" + data.getLocalizacion().getTipoVia() + "' , '"
+                    + data.getLocalizacion().getVia() + "','" + data.getLocalizacion().getNum() + "','" + data.getLocalizacion().getProvincia() + "'.'"
+                    + data.getLocalizacion().getCodigoPostal() + "','" + data.getLocalizacion().getPais() + "','" + data.getLocalizacion().getObservaciones() + "','"
+                    + data.getFechaInicio() + "','" + data.getSocioLocal() + "','" + data.getAccionesRealizar() + "','" + data.getLineaAccion() + "','" + data.getSublineaAccion() +")";
+            stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("No se pudo insertar el proyecto");
+        }
 
     }
 }
